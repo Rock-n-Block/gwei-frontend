08f7f6546f5d682cf3d08297cf54ee0894a3beb2
Daniil <rusm9snik2013@yandex.ru>
2022-01-19 15:03:40 +0300

    fix heroku, styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 // import legacy from '@vitejs/plugin-legacy';
+import inject from '@rollup/plugin-inject';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -12,6 +13,14 @@ export default defineConfig({
       root: './',
     }),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   resolve: {
     alias: {
       web3: path.resolve(__dirname, './node_modules/web3/dist/web3.min.js'),

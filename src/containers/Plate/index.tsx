1a9c6a2d994e633d5a12c2e0eabea9da2b4889ea
KiryaFT <uper99@gmail.com>
2022-01-24 15:01:45 +0300

    Fix footer, lint Plate

--- src/containers/Plate/index.tsx
+++ src/containers/Plate/index.tsx
@@ -1,18 +1,16 @@
 import { FC, memo } from 'react';
-import s from './Plate.module.scss';
+
 import cn from 'classnames';
+
+import s from './Plate.module.scss';
 
 interface PlateI {
   children: JSX.Element | JSX.Element[];
   className?: string;
 }
 
-const Plate: FC<PlateI>  = ({ children , className}) => {
-  return (
-    <div className={cn(s.plate, className)}>
-      {children}
-    </div>
-  );
+const Plate: FC<PlateI> = ({ children, className }) => {
+  return <div className={cn(s.plate, className)}>{children}</div>;
 };
 
 export default memo(Plate);
